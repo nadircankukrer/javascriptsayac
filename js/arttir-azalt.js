dc09283efbb3fb26c3d223126ddf893bae81c2a3
Nadircan Kükrer <115514788+nadircankukrer@users.noreply.github.com>
2023-07-21 04:41:45 +0300

    sıfırla butonu eklendi

--- js/arttir-azalt.js
+++ js/arttir-azalt.js
@@ -2,15 +2,19 @@ let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('cou
 let counterDOM = document.querySelector('#counter')
 let increaseDOM = document.querySelector('#increase')
 let decreaseDOM = document.querySelector('#decrease')
+let sifirlaBtn = document.querySelector('#sifirla')
 
 counterDOM.innerHTML = counter
 
 increaseDOM.addEventListener("click", clickEvent)
 decreaseDOM.addEventListener("click", clickEvent)
+sifirlaBtn.addEventListener("click", clickEvent)
 
-function clickEvent (){
+function clickEvent() {
     console.log(this.id)
-    this.id == "increase" ? counter += 1 : counter -= 1
+    if (this.id == "increase") { counter += 1 }
+    else if (this.id == "decrease") { counter -= 1 }
+    else if (this.id == "sifirla") { counter = 0 }
     localStorage.setItem('counter', counter)
     counterDOM.innerHTML = counter
 }
